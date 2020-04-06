72ea7eeb07dfc66ba1769dd65e5100a3c5c020fc
pengpengker <pengpengker9@gmail.com>
2020-04-06 20:30:31 +0800

    wechat修复jsapi

--- application/common/pay/WxJsApi.php
+++ application/common/pay/WxJsApi.php
@@ -7,6 +7,9 @@ namespace app\common\pay;
 
 use think\Db;
 use think\Request;
+use app\common\Pay;
+use think\Loader;
+use think\Session;
 
 class WxJsApi extends Pay{
     protected $code='';
@@ -32,28 +35,41 @@ class WxJsApi extends Pay{
     {
         if (strpos(@$_SERVER['HTTP_USER_AGENT'], 'MicroMessenger') !== false) {
             $pay_type = 'JSAPI';
-            \think\Loader::import('wxpay.lib.WxPayApi');
-            \think\Loader::import('wxpay.JsApiPay');
-            $tools = new JsApiPay();
-            $openId = $tools->GetOpenid();
-            halt(GetOpenid());
+            $openid = Session::get('openid');
+            Loader::import('wxpay.WxPayJsApiPay');
+            Loader::import('wxpay.WxPayApi.php');
+            Loader::import('wxpay.WxPayConfig.php');
+            Loader::import('wxpay.WxPayJsApiPay');
+            $tools = new \JsApiPay();
+            if(empty(Session::get('openid'))){
+	            $data = $tools->GetOpenid();
+	            $openid = $data['openid'];
+	            Session::set('openid',$openid);
+            }
+            $input = new \WxPayUnifiedOrder();
+			$input->SetBody($subject);
+			$input->SetAttach($this->createNoncestr());
+			$input->SetOut_trade_no($outTradeNo);
+			$input->SetTotal_fee($totalAmount * 100);
+			$input->SetTime_start(date("YmdHis"));
+			$input->SetTime_expire(date("YmdHis", time() + 600));
+			$input->SetGoods_tag($subject);
+			$input->SetNotify_url(Request::instance()->domain().'/pay/notify/WxJsApi');
+			$input->SetTrade_type("JSAPI");
+			$input->SetOpenid($openid);
+			$this->defineWxConfig($this->account->params);
+			$config = new \WxPayConfig();
+			$order = \WxPayApi::unifiedOrder($config, $input);
+			$jsApiParameters = $tools->GetJsApiParameters($order);
+			$editAddress = $tools->GetEditAddressParameters();
+			$result = $jsApiParameters;
         }else{
             $pay_type = 'MWEB';
         }
-
-        $params = [
-            'body' => $subject,
-            'out_trade_no' => $outTradeNo,
-            'total_fee' => $totalAmount*100,
-        ];
-
-        $result = \wxpay\JsapiPay::getPayUrl($params);
-        halt($result);
-
         $this->code    =0;
         $obj           =new \stdClass();
         $obj->pay_url  =$result;
-        $obj->content_type = 2;
+        $obj->content_type = 5;
         return $obj;
     }
 
@@ -129,6 +145,72 @@ class WxJsApi extends Pay{
             return $result;
         }else{
             return false;
+        }
+    }
+    function createNoncestr( $length = 32 ){
+        $chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+        $str ="";
+        for ( $i = 0; $i < $length; $i++ )  {
+            $str.= substr($chars, mt_rand(0, strlen($chars)-1), 1);
+        }
+        return $str;
+    }
+    function postXmlCurl($xml,$url,$second = 30){
+        $ch = curl_init();
+        //设置超时
+        curl_setopt($ch, CURLOPT_TIMEOUT, $second);
+        curl_setopt($ch,CURLOPT_URL, $url);
+        curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,FALSE);
+        curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,FALSE);
+        //设置header
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        //要求结果为字符串且输出到屏幕上
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        //post提交方式
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
+        //运行curl
+        $data = curl_exec($ch);
+        //返回结果
+        if($data){
+            curl_close($ch);
+            return $data;
+        }else{
+            $error = curl_errno($ch);
+            curl_close($ch);
+            return "Error";
+        }
+    }
+    function get_client_ip() {
+        if(getenv('HTTP_CLIENT_IP') && strcasecmp(getenv('HTTP_CLIENT_IP'), 'unknown')) {
+            $ip = getenv('HTTP_CLIENT_IP');
+        } elseif(getenv('HTTP_X_FORWARDED_FOR') && strcasecmp(getenv('HTTP_X_FORWARDED_FOR'), 'unknown')) {
+            $ip = getenv('HTTP_X_FORWARDED_FOR');
+        } elseif(getenv('REMOTE_ADDR') && strcasecmp(getenv('REMOTE_ADDR'), 'unknown')) {
+            $ip = getenv('REMOTE_ADDR');
+        } elseif(isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'], 'unknown')) {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        return $ip;
+    }
+    private function defineWxConfig($pargrm) {
+        if (!defined('wx_APPID')) {
+            define('wx_APPID', $pargrm->APPID);
+        }
+        if (!defined('wx_MCHID')) {
+            define('wx_MCHID', $pargrm->MCHID);
+        }
+        if (!defined('wx_SUBAPPID')) {
+            define('wx_SUBAPPID', @$pargrm->sub_appid);
+        }
+        if (!defined('wx_SUBMCHID')) {
+            define('wx_SUBMCHID', @$pargrm->sub_mch_id);
+        }
+        if (!defined('wx_KEY')) {
+            define('wx_KEY', $pargrm->KEY);
+        }
+        if (!defined('wx_APPSECRET')) {
+            define('wx_APPSECRET', $pargrm->APPSECRET);
         }
     }
 }
